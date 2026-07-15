d93759b1e26e2abb763a12674378b909ce5b2d35
Hidy <roiyeon@gmail.com>
2026-07-15 13:39:41 +0900

    chore(lint): add type-aware eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['src/**/*.ts'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+});
